de8c0f55625bbc2ad10c75365d1955d72ec345e5
bkarakadioglu <batuhankarakadioglu@gmail.com>
2022-11-19 23:08:53 +0300

    Add alt to img for team logo

--- src/components/TeamTableRow.js
+++ src/components/TeamTableRow.js
@@ -3,7 +3,7 @@ export default class TeamTableRow extends Component {
     render() {
         return (
             <tr>
-                <td><img src= {this.props.obj.team.logo}/></td>
+                <td><img src= {this.props.obj.team.logo} alt={this.props.obj.team.name + "logo"}/></td>
                 <td>{this.props.obj.team.name}</td>
                 <td>{this.props.obj.team.founded}</td>
                 <td>{this.props.obj.venue.city}</td>
