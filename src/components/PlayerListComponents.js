edd78837d5564fe3622fb73a770a68fbf6de88a6
erdemkaratash <erdemkaratash@gmail.com>
2022-11-21 23:01:39 +0300

    Remove paginatonProvider to allow deploy

--- src/components/PlayerListComponents.js
+++ src/components/PlayerListComponents.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
-import paginationFactory, { PaginationProvider } from 'react-bootstrap-table2-paginator';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 export default class PlayerList extends Component {
   constructor(props) {
